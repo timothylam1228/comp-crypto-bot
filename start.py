fbf0ffe39d7a1eaf2d32f24bb440fe7d3d73709b
Timothy Lam <timothylam1228@gmail.com>
2021-01-11 22:00:00 +0800

    Update start.py

--- start.py
+++ start.py
@@ -38,42 +38,6 @@ def start(update, context):
     """Send a message when the command /start is issued."""
     update.message.reply_text('/source for Geting the source\n/canteen to show canteen')
 
-def newmember(update, context):
-    """Send a message when the command /help is issued."""
-    query = update.callback_query
-    bot = context.bot
-    url = helpers.create_deep_linked_url(bot.get_me().username, SO_COOL)
-    text = "歡迎來到IT谷" 
-    keyboard = InlineKeyboardMarkup.from_button(
-        InlineKeyboardButton(text='Continue here!', url=url)
-        )
-    ogg_url = 'https://github.com/timothylam1228/CodeDeployGitHubDemo/raw/master/source/plato.ogg'
-    for member in update.message.new_chat_members:
-        update.message.reply_text(text, reply_markup=keyboard)
-        context.bot.send_voice(chat_id = update.message.chat.id, voice = ogg_url)
-        new_members = update.message.new_chat_members
-        firstname = member.first_name
-        lastname = member.last_name
-        if("+852" in firstname) :
-            bot.kick_chat_member(chat_id=update.message.chat.id, user_id=update.message.from_user.id)
-            return
-        elif("+852" in lastname):
-            bot.kick_chat_member(chat_id=update.message.chat.id, user_id=update.message.from_user.id)
-            return
-
-    
-    
-def open_bot(update, context):
-    x = update.message.from_user.id
-    print(x)
-    bot = context.bot
-    url = helpers.create_deep_linked_url(bot.get_me().username, SO_COOL)
-    keyboard = InlineKeyboardMarkup.from_button(
-        InlineKeyboardButton(text='Get Resource here!', url=url)
-    )
-    #context.bot.sendMessage(chat_id=update.message.chat.id,text ="        Hello",reply_markup=keyboard)
-    update.message.reply_text("Hello", reply_markup=keyboard)
-
 
 def button(update, context):
     query = update.callback_query
